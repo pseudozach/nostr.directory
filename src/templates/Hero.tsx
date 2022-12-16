77c529514c03fd8f83835b873233cb91f03b2005
pseudozach <git@pseudozach.com>
2022-12-16 10:17:02 -0800

    add #nostr to tweet

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -44,7 +44,7 @@ const Hero = () => (
         }
         description="Add your nostr public key to the directory so your followers can find you"
         button={
-          <Link href="https://twitter.com/intent/tweet?text=Verifying%20my%20account%20on%20nostr%0A%0AMy%20Public%20Key%3A%20%22Paste%20your%20nostr%20public%20key%20here%22%0A%0AFind%20others%20at%20https%3A%2F%2Fnostr.directory%20%40nostrdirectory">
+          <Link href="https://twitter.com/intent/tweet?text=Verifying%20my%20account%20on%20nostr%0A%0AMy%20Public%20Key%3A%20%22Paste%20your%20nostr%20public%20key%20here%22%0A%0AFind%20others%20at%20https%3A%2F%2Fnostr.directory%20%40nostrdirectory%20%23nostr">
             <a target="_blank" rel="noreferrer">
               <Button xl>Tweet your nostr pubkey</Button>
             </a>
