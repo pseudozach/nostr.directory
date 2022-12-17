22faebb63b82e59aa7fb819aa008750905947533
pseudozach <git@pseudozach.com>
2022-12-16 23:03:38 -0800

    update text

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -38,17 +38,38 @@ const Banner = () => {
     name: 'coracle.social',
     link: 'https://coracle.social',
   });
+
   useEffect(() => {
     const randomClient =
       nostrClientArray[Math.floor(Math.random() * nostrClientArray.length)] ||
       client;
     setClient(randomClient);
   }, [client]);
+
+  // dont change the link every 5 seconds - bad idea
+  // useEffect(() => {
+  //   if (currentIndex === nostrClientArray.length - 1) {
+  //     console.log('stopping');
+  //     return;
+  //   }
+  //   console.log('useEffect');
+  //   const interval = setInterval(() => {
+  //     const updatedData = currentIndex + 1;
+  //     setCurrentIndex(updatedData);
+  //     setClient(nostrClientArray[updatedData]!);
+  //     console.log('updated to ', nostrClientArray[updatedData]);
+  //   }, 5000);
+
+  //   console.log('clearInterval');
+  //   // eslint-disable-next-line consistent-return
+  //   return () => clearInterval(interval);
+  // }, [currentIndex]);
+
   return (
     <Section>
       <CTABanner
         title="Nostr is a distributed social media protocol that has a chance of working."
-        subtitle="Give it a try."
+        subtitle="Here's a random client. Give it a try."
         button={
           <Link href={client.link}>
             <a target="_blank">
