51749016d04a810ad119f18c94f1a85fdbd9bdc9
pseudozach <git@pseudozach.com>
2022-12-19 19:34:04 -0800

    allow cors

--- api/nostr.ts
+++ api/nostr.ts
@@ -29,6 +29,20 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
+  // allow cors
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
   // nostr nip05: https://<domain>/.well-known/nostr.json?name=<local-part>
   try {
     if (!req.query.name) {
